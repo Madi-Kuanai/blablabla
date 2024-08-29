9decf1537bef72b803a218e924e6b3606281c6c2
Kuanai Madi <80389284+Madi-Kuanai@users.noreply.github.com>
2024-08-30 01:59:27 +0500

    basic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 
 function App() {
   const [text, setText] = useState('');
+
   useEffect(() => {
     if (!window.Telegram) {
       console.error('Telegram WebApp API не доступен');
@@ -11,38 +12,34 @@ function App() {
 
   const onSendData = useCallback(() => {
     const tg = window.Telegram ? window.Telegram.WebApp : null;
-    console.log("web")
     if (tg) {
-      const user = tg.initDataUnsafe.user;
+      const user = tg.initDataUnsafe?.user;
       const data = {
         text,
         user: {
-          firstName: user?.first_name,
-          lastName: user?.last_name,
+          first_name: user?.first_name,
+          last_name: user?.last_name,
           username: user?.username,
         },
       };
-      tg.sendData(JSON.stringify(data));
-
-      console.log(tg)
-      alert("Ваше сообщение получено")
+      tg.sendData(JSON.stringify(data)); // Отправка данных в формате JSON
     } else {
       console.error('Telegram WebApp API не доступен');
     }
   }, [text]);
 
   return (
-    <div className="App">
+      <div className="App">
       <textarea
-        className="text-box"
-        placeholder="Введите текст..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+          className="text-box"
+          placeholder="Введите текст..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
       ></textarea>
-      <button className="submit-button" onClick={onSendData}>
-        Отправить
-      </button>
-    </div>
+        <button className="submit-button" onClick={onSendData}>
+          Отправить
+        </button>
+      </div>
   );
 }
 
