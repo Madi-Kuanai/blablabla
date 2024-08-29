3394ddbc0cf6d41147bfd39b30021758a3fcd974
Kuanai Madi <80389284+Madi-Kuanai@users.noreply.github.com>
2024-08-30 03:16:08 +0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import { useEffect, useState, useCallback } from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import "./App.css"
 import {Header} from "./components/Header";
 
 function App() {
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    if (!window.Telegram) {
-      console.error('Telegram WebApp API не доступен');
-    }
-  }, []);
-
-  const onSendData = useCallback(() => {
+    const [text, setText] = useState('');
     const tg = window.Telegram ? window.Telegram.WebApp : null;
     const user = tg.initData?.user;
+    console.log("User data:", tg.initDataUnsafe.user);
+    useEffect(() => {
+        if (!window.Telegram) {
+            console.error('Telegram WebApp API не доступен');
+        }
+    }, []);
+
+    const onSendData = useCallback(() => {
 
 
-    if (tg) {
-      const data = {
-        text,
-        user: {
-          first_name: user?.first_name,
-          last_name: user?.last_name,
-          username: user?.username,
-        },
-      };
-      setTimeout(() => {
-        alert(user?.first_name);
-      }, 1000);
+        if (tg) {
+            const data = {
+                text,
+                user: {
+                    first_name: user?.first_name,
+                    last_name: user?.last_name,
+                    username: user?.username,
+                },
+            };
+            setTimeout(() => {
+                alert(user?.first_name);
+            }, 1000);
 
-      tg.sendData(JSON.stringify(data));
+            tg.sendData(JSON.stringify(data));
 
-    } else {
-      console.error('Telegram WebApp API не доступен');
-    }
-  }, [text]);
+        } else {
+            console.error('Telegram WebApp API не доступен');
+        }
+    }, [text]);
 
-  return (
-      <div className="App">
-        <Header/>
-      <textarea
-          className="text-box"
-          placeholder="Введите текст..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-      ></textarea>
-        <button className="submit-button" onClick={onSendData}>
-          Отправить
-        </button>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <textarea
+                className="text-box"
+                placeholder="Введите текст..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            ></textarea>
+            <button className="submit-button" onClick={onSendData}>
+                Отправить
+            </button>
+        </div>
+    );
 }
 
 export default App;
