1051fbdee6934969a072f2c41f07c0d8f728f15e
Kuanai Madi <80389284+Madi-Kuanai@users.noreply.github.com>
2024-08-29 23:27:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import {Header} from './components/Header'
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleSubmit = () => {
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    window.Telegram.WebApp.sendData(text);
+    window.Telegram.WebApp.sendData(user);
+    
+  };
   return (
     <>    <div className="App">
       {<Header/>}
-      <textarea className="text-box" placeholder="Введите текст..."></textarea>
-      <button className="submit-button">Отправить</button>
+      <textarea
+          className="text-box"
+          placeholder="Введите текст..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <button className="submit-button" onClick={handleSubmit}>
+          Отправить
+        </button>
     </div>
     </>
   );
