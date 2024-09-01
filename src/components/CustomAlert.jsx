e9676d361667601f1e163132b69b93304bfb4d98
Kuanai Madi <80389284+Madi-Kuanai@users.noreply.github.com>
2024-09-02 01:58:27 +0500

    basic

--- src/components/CustomAlert.jsx
+++ src/components/CustomAlert.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Lottie from "react-lottie";
 import loadingAnimationData from '../assets/alert-animation.json';
 import okAnimationData from '../assets/ok-animation.json';
-import './CustomAlert.css';
+import '../CustomAlert.css';
 
 export function CustomAlert({ isEnd, isError, onButtonClick }) {
     const defaultOptions = {
